46eeddf8dbef6c683c417dd9259b0250a3e3300c
Bryan Hurtado Delgado <bryanhurtadodelgado@gmail.com>
2025-05-25 20:56:23 -0500

    feat(app): agregar styles, theme, normalize y global

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/App.tsx';
+import '@/app/styles/index.scss';
+
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
